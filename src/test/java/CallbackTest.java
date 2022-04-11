8db3e95f5be3f8b7e600bd10a5ff8679f8c979f3
Artem Sukhanov <IceMan161@yandex.ru>
2022-04-11 22:33:08 +0300

    fix test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -24,7 +24,11 @@ public class CallbackTest {
 
     @BeforeEach
     void setupTest() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
